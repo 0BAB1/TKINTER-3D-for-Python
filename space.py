8137669444fd70f6509d9c0f945c301a2487e466
0BAB1 <62248962+0BAB1@users.noreply.github.com>
2021-07-04 00:04:37 +0200

    rotations are ON

--- space.py
+++ space.py
@@ -5,18 +5,70 @@ class Space:
     def __init__(self):
         #adding the dots array, dots will be x,y,z tuples
         self.dots=[]
-        self.add_square(10,10,10,100,100,200)
+        self.add_square(100,200,10, 200,300,200)
+        self.rotateY()
+        self.rotateX()
+    
+    def rotateY(self, angle=1):
+        '''this method will rotate all the dots around y0 axis with the help of rotation matrix'''
         #the matrix to enable rotation around Y
-        self.rotation =[
-            [1,0,0],
-            [0,1,0],
+        rotationY =[
+            [math.cos(angle),0,math.sin(angle)],
+            [0              ,1              ,0],
+            [-math.sin(angle),0,math.cos(angle)]
+        ]
+        for i in range(len(self.dots)):
+            # we are doing a base change :
+            # syntax // <newcoord> = <matrixCoord>*<oldCoord> <-- repeat for x y and z
+            # NewX = 00*0(Old x) + 01*1(Old y) + 02*2(Old z)
+            # NewY = 10*0 + 11*1 + 12*2
+            # NewZ = 20*0 + 21*1 + 22*2
+            new_x=rotationY[0][0]*self.dots[i][0]+rotationY[0][1]*self.dots[i][1]+rotationY[0][2]*self.dots[i][2]
+            new_y=rotationY[1][0]*self.dots[i][0]+rotationY[1][1]*self.dots[i][1]+rotationY[1][2]*self.dots[i][2]
+            new_z=rotationY[2][0]*self.dots[i][0]+rotationY[2][1]*self.dots[i][1]+rotationY[2][2]*self.dots[i][2]
+            #apply new coords to dots
+            self.dots[i] = (new_x, new_y, new_z)
+
+    def rotateX(self, angle=1):
+        '''this method will rotate all the dots around x0 axis with the help of rotation matrix'''
+        #the matrix to enable rotation around Y
+        rotationX =[
+            [1,           0,                  0],
+            [0,math.cos(angle),-math.sin(angle)],
+            [0,math.sin(angle),math.cos(angle)]
+        ]
+        for i in range(len(self.dots)):
+            # we are doing a base change :
+            # syntax // <newcoord> = <matrixCoord>*<oldCoord> <-- repeat for x y and z
+            # NewX = 00*0(Old x) + 01*1(Old y) + 02*2(Old z)
+            # NewY = 10*0 + 11*1 + 12*2
+            # NewZ = 20*0 + 21*1 + 22*2
+            new_x=rotationX[0][0]*self.dots[i][0]+rotationX[0][1]*self.dots[i][1]+rotationX[0][2]*self.dots[i][2]
+            new_y=rotationX[1][0]*self.dots[i][0]+rotationX[1][1]*self.dots[i][1]+rotationX[1][2]*self.dots[i][2]
+            new_z=rotationX[2][0]*self.dots[i][0]+rotationX[2][1]*self.dots[i][1]+rotationX[2][2]*self.dots[i][2]
+            #apply new coords to dots
+            self.dots[i] = (new_x, new_y, new_z)
+    
+    def rotateZ(self, angle=1):
+        '''this method will rotate all the dots around x0 axis with the help of rotation matrix'''
+        #the matrix to enable rotation around Y
+        rotationZ =[
+            [math.cos(angle),-math.sin(angle),0],
+            [math.sin(angle),math.cos(angle),0],
             [0,0,1]
         ]
-    
-    def rotate(self, angle):
-        '''this method will rotate all the dots around y0 axis with the help of rotation matrix'''
-        for dot in self.dots:
-            pass
+        for i in range(len(self.dots)):
+            # we are doing a base change :
+            # syntax // <newcoord> = <matrixCoord>*<oldCoord> <-- repeat for x y and z
+            # NewX = 00*0(Old x) + 01*1(Old y) + 02*2(Old z)
+            # NewY = 10*0 + 11*1 + 12*2
+            # NewZ = 20*0 + 21*1 + 22*2
+            new_x=rotationZ[0][0]*self.dots[i][0]+rotationZ[0][1]*self.dots[i][1]+rotationZ[0][2]*self.dots[i][2]
+            new_y=rotationZ[1][0]*self.dots[i][0]+rotationZ[1][1]*self.dots[i][1]+rotationZ[1][2]*self.dots[i][2]
+            new_z=rotationZ[2][0]*self.dots[i][0]+rotationZ[2][1]*self.dots[i][1]+rotationZ[2][2]*self.dots[i][2]
+            #apply new coords to dots
+            self.dots[i] = (new_x, new_y, new_z)
+            
     
     def add_square(self, x1,y1,z1, x2,y2,z2):
         '''this method add a square to our vectorial space'''
