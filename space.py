1ca68883037edbeb38524c36886965288cae920d
0BAB1 <62248962+0BAB1@users.noreply.github.com>
2021-07-03 20:50:05 +0200

    get a side view of our square

--- space.py
+++ space.py
@@ -3,3 +3,23 @@ class Space:
     def __init__(self):
         #adding the dots array, dots will be x,y,z tuples
         self.dots=[]
+        self.add_square(10,10,10,100,100,200)
+    
+    def add_square(self, x1,y1,z1, x2,y2,z2):
+        '''this method add a square to our vectorial space'''
+        # here on x axis , adding 1 dot/pixel, as it is a cube, 4 edeges go the same way, so we do it 4x, 
+        # in Y=y1 / Z=z1 ,
+        # then in Y=y2 / Z=z1,
+        # then in Y=y1 / Z=z2
+        # and finally on Y=y2 and Z=z2
+        for i in range(x1, x2):
+            #Y=y1 Z=1
+            self.dots.append((i,y1,z1))
+            #Y=y2 Z=z1
+            self.dots.append((i,y2,z1))
+            #Y=y1 Z=z2
+            self.dots.append((i,y1,z2))
+            #Y=y2 Z=z2
+            self.dots.append((i,y2,z2))
+        
+        #we now proceed to do the same on Y and Z
