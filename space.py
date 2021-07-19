96f5e2994502fb56eadef9352e2419fb7c32bc19
0BAB1 <62248962+0BAB1@users.noreply.github.com>
2021-07-19 16:10:38 +0200

    saveday

--- space.py
+++ space.py
@@ -20,6 +20,12 @@ def rotation(vector,x,y,z):
         [0,0,1]
     ]
 
+    # we are doing a base change :
+    # syntax // <newcoord> = <matrixCoord>*<oldCoord> <-- repeat for x y and z
+    # NewX = 00*0(Old x) + 01*1(Old y) + 02*2(Old z)
+    # NewY = 10*0 + 11*1 + 12*2
+    # NewZ = 20*0 + 21*1 + 22*2
+
     if not x==0:
         #x = 1*x <=> vect[0] = vect[0]
         vect[1]=rotationX[1][0]*vect[0]+rotationX[1][1]*vect[1]+rotationX[1][2]*vect[2]
@@ -56,82 +62,26 @@ class Space:
     
     def del_shape(self, name):
         self.dots.pop(name)
-        self.triangles.pop(name)
+        self.meshes.pop(name)
 
     def rotate(self,x,y,z):
-        '''to rotate every shapes around X0, Y0 and Z0 axis'''
-        self.rotations = (self.rotations[0]+x,self.rotations[1]+y,self.rotations[2]+z)
-        rotationY =[
-            [math.cos(y),0,math.sin(y)],
-            [0 ,1,0],
-            [-math.sin(y),0,math.cos(y)]
-        ]
-        rotationX =[
-            [1,0,0],
-            [0,math.cos(x),-math.sin(x)],
-            [0,math.sin(x),math.cos(x)]
-        ]
-        rotationZ =[
-            [math.cos(z),-math.sin(z),0],
-            [math.sin(z),math.cos(z),0],
-            [0,0,1]
-        ]
-
-        if not x==0:
-            for name, value in self.dots.items(): #here, a value is a group of point associated to a shape
-            #a value's structure : [(x,y,z,color) , (x,y,z,color), ...]
-                for i in range(len(value)):
-                    new_y=rotationX[1][0]*self.dots[name][i][0]+rotationX[1][1]*self.dots[name][i][1]+rotationX[1][2]*self.dots[name][i][2]
-                    new_z=rotationX[2][0]*self.dots[name][i][0]+rotationX[2][1]*self.dots[name][i][1]+rotationX[2][2]*self.dots[name][i][2]
-
-                    self.dots[name][i] = (self.dots[name][i][0], new_y, new_z, self.dots[name][i][3]) #the new x is the old one
-
-            for i in range(len(self.origin_dots)):
-                #   this line is optional as it is the rotation axis new_x=rotationX[0][0]*self.origin_dots[i][0]+rotationX[0][1]*self.origin_dots[i][1]+rotationX[0][2]*self.origin_dots[i][2]
-                new_y=rotationX[1][0]*self.origin_dots[i][0]+rotationX[1][1]*self.origin_dots[i][1]+rotationX[1][2]*self.origin_dots[i][2]
-                new_z=rotationX[2][0]*self.origin_dots[i][0]+rotationX[2][1]*self.origin_dots[i][1]+rotationX[2][2]*self.origin_dots[i][2]
-                self.origin_dots[i] = (self.origin_dots[i][0], new_y, new_z) #the new x is the old one
+        '''to rotate every shape around x y or / and z'''
+        for name, value in self.dots.items(): #here, a value is a group of point associated to a shape
+        #a value's structure : [(x,y,z,color) , (x,y,z,color), ...]
+            for i in range(len(value)):
+                newCoords = rotation((i[0],i[1],i[2]),x,y,z)
+                self.dots[name][i]=(newCoords[0],newCoords[1],newCoords[2],self.dots[name][i][3])
         
-        if not y==0:
-            for name, value in self.dots.items(): #here, a value is a group of point associated to a shape
-            #a value's structure : [(x,y,z,color) , (x,y,z,color), ...]
-                for i in range(len(value)):
-                    # we are doing a base change :
-                    # syntax // <newcoord> = <matrixCoord>*<oldCoord> <-- repeat for x y and z
-                    # NewX = 00*0(Old x) + 01*1(Old y) + 02*2(Old z)
-                    # NewY = 10*0 + 11*1 + 12*2
-                    # NewZ = 20*0 + 21*1 + 22*2
-                    new_x=rotationY[0][0]*self.dots[name][i][0]+rotationY[0][1]*self.dots[name][i][1]+rotationY[0][2]*self.dots[name][i][2]
-                    new_z=rotationY[2][0]*self.dots[name][i][0]+rotationY[2][1]*self.dots[name][i][1]+rotationY[2][2]*self.dots[name][i][2]
-                    #apply new coords to dots
-                    self.dots[name][i] = (new_x, self.dots[name][i][1], new_z, self.dots[name][i][3])
-            
-            for name, mesh in self.meshes.items():
-                #rotation for triangles in mesh
-                # memo : mesh : [ (color,(dot1),(dot2),(dot3)) , (color,(dot1),(dot2),(dot3))]
-                #      ^^list of triangles    ^^this is triangle 1          ^^ this is triangle 2    .. etc
-                for i in range(len(mesh)):
-                    pass
-            
-            #this time for origin dots (to improve btw)
-            for i in range(len(self.origin_dots)):
-                new_x=rotationY[0][0]*self.origin_dots[i][0]+rotationY[0][1]*self.origin_dots[i][1]+rotationY[0][2]*self.origin_dots[i][2]
-                new_z=rotationY[2][0]*self.origin_dots[i][0]+rotationY[2][1]*self.origin_dots[i][1]+rotationY[2][2]*self.origin_dots[i][2]
-                self.origin_dots[i] = (new_x, self.origin_dots[i][1], new_z)
+        for name, mesh in self.meshes.items():
+            #rotation for triangles in mesh
+            # memo : mesh : [ (color,(dot1),(dot2),(dot3)) , (color,(dot1),(dot2),(dot3))]
+            #      ^^list of triangles    ^^this is triangle 1          ^^ this is triangle 2    .. etc
+            for i in range(len(mesh)):
+                pass
         
-        if not z==0:
-            for name, value in self.dots.items(): #here, a value is a group of point associated to a shape
-            #a value's structure : [(x,y,z,color) , (x,y,z,color), ...]
-                for i in range(len(value)):
-                    new_x=rotationZ[0][0]*self.dots[name][i][0]+rotationZ[0][1]*self.dots[name][i][1]+rotationZ[0][2]*self.dots[name][i][2]
-                    new_y=rotationZ[1][0]*self.dots[name][i][0]+rotationZ[1][1]*self.dots[name][i][1]+rotationZ[1][2]*self.dots[name][i][2]
-
-                    self.dots[name][i] = (new_x, new_y, self.dots[name][i][2], self.dots[name][i][3])
-            
-            for i in range(len(self.origin_dots)):
-                new_x=rotationZ[0][0]*self.origin_dots[i][0]+rotationZ[0][1]*self.origin_dots[i][1]+rotationZ[0][2]*self.origin_dots[i][2]
-                new_y=rotationZ[1][0]*self.origin_dots[i][0]+rotationZ[1][1]*self.origin_dots[i][1]+rotationZ[1][2]*self.origin_dots[i][2]
-                self.origin_dots[i] = (new_x, new_y, self.origin_dots[i][2])
+        #this time for origin dots (to improve btw)
+        for i in range(len(self.origin_dots)):
+            pass
 
     def add_line(self,x1,y1,z1, x2,y2,z2,name,color="black"):
         '''to add a simple line and create custom shapes'''
