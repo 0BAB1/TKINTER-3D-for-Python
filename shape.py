e53e0079ce58a2e0a018a44fddf6f75477d560ce
0BAB1 <62248962+0BAB1@users.noreply.github.com>
2021-07-27 14:47:59 +0200

    minor chage

--- shape.py
+++ shape.py
@@ -5,14 +5,6 @@ class Shape:
     '''this class create a shape, with a transfomation tracker'''
     def __init__(self, id="none"):
         self.id = id
-        self.transformation = {
-            "Tx" : 0,
-            "Ty" : 0,
-            "Tz" : 0,
-            "Rx" : 0,
-            "Ry" : 0,
-            "Rz" : 0
-        }
         self.position = (0,0,0) # the shape's postion relative to the main origin (middle of the screen)
         self.mesh = [] #an array of triangles : [triangle1 , triangle2 , etc...]
         #atriangle looks like this : [color, outline, (xyz),(xyz),(xyz)]
